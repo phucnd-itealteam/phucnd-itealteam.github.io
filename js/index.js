dbfeae0979614309a100234e9570a7416b54e901
phucnd-itealteam <phucnd.iteal@gmail.com>
2023-04-19 13:47:44 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,155 @@
+import {keyLanguage} from "./translateKey.js";
+
+const LINK_APPSTORE_MOBILE = 'https://vigtek.page.link/?link=https://tictop.app/apn=vig.tictop.app&ibi=vig.tictop.app01&isi=1582398478';
+const LINK_GOOGLE_PLAY_MOBILE = 'https://vigtek.page.link/?link=https://tictop.app/ibi=vig.tictop.app01&isi=1582398478&apn=vig.tictop.app';
+const LINK_APPSTORE_WEB = 'https://apps.apple.com/vn/app/tictop/id1582398478';
+const LINK_GOOGLE_PLAY_WEB = 'https://play.google.com/store/apps/details?id=vig.tictop.app';
+
+
+// Setting href button register
+const setLinkToButtonRegister = () => {
+    const md = new MobileDetect(window.navigator.userAgent);
+    const btn_appStore = document.getElementById('tictop-btn__appstore');
+    const btn_googleplay = document.getElementById('tictop-btn__googleplay');
+
+    btn_appStore.href = LINK_APPSTORE_MOBILE;
+    btn_googleplay.href = LINK_GOOGLE_PLAY_MOBILE;
+
+    if (md.mobile()) {
+        if (md.os() === 'iOS') {
+            btn_googleplay.style.display = 'none';
+        } else {
+            btn_appStore.style.display = 'none';
+        }
+    } else {
+        btn_appStore.href = LINK_APPSTORE_WEB;
+        btn_googleplay.href = LINK_GOOGLE_PLAY_WEB;
+    }
+};
+
+// Get Language
+const detectLanguage = () => {
+// 1. Get from local storage
+    const userSelectedLanguage = localStorage.getItem('LANGUAGE');
+    if (userSelectedLanguage && systemSupportLanguage(userSelectedLanguage)) {
+        return userSelectedLanguage;
+    }
+
+// 2. Get browser's language
+    const browserLanguage = window.navigator.language;
+    if (systemSupportLanguage(browserLanguage)) {
+        localStorage.setItem('LANGUAGE', getLanguageByCountryCode(browserLanguage));
+        return browserLanguage;
+    }
+
+// 3. Get location via IP
+    const ipInfoData = getIpInfo();
+    const langFromIp = getLanguageByCountryCode(ipInfoData?.countryCode);
+    if (systemSupportLanguage(langFromIp)) {
+        localStorage.setItem('LANGUAGE', langFromIp);
+        return langFromIp;
+    }
+
+// 4. By default
+    return 'vi';
+
+};
+
+const getIpInfo = () => {
+    let locationData;
+    $(function () {
+        $.ajax({
+            url: "https://api.tictop.app/api/web/Setting/GetIpInfo",
+            success: function (data) {
+                locationData = data['result'];
+                return locationData;
+            },
+            error: function (error) {
+                console.log('error', error);
+            }
+        });
+    });
+    return 'vi';
+};
+
+const getLanguageByCountryCode = (countryCode) => {
+    let defaultLanguage = 'en';
+    switch (countryCode) {
+        case 'EU', 'en-US':
+            defaultLanguage = 'en';
+            break;
+        case 'VN':
+            defaultLanguage = 'vi';
+            break;
+
+        default:
+            defaultLanguage = 'en';
+            break;
+    }
+    return defaultLanguage;
+};
+
+const systemSupportLanguage = (lang) => {
+    if (!lang) return false;
+    const convertLang = lang.slice(0, 2).toLowerCase();
+    return ['en', 'vi', 'fr'].some((o) => o === convertLang);
+};
+
+const translate = () => {
+    const language = detectLanguage() || 'vi';
+    const languageData = keyLanguage;
+    document.querySelectorAll('[data-translate]').forEach((el) => {
+        let key = el.attributes['data-translate']?.value;
+        el.innerHTML = languageData[key][language] || ' ';
+    });
+};
+
+// Link Website In Footer
+const onSetLinkRedirectWeb = () => {
+    const language = detectLanguage() || 'vi';
+    const els = document.querySelectorAll('.link_website');
+    els.forEach(el => {
+        if (language !== 'fr') el.href = 'https://tictop.vn';
+        else el.href = 'https://tictop.eu';
+    });
+};
+
+// On change Language
+const initSelectElement = () => {
+    const language = detectLanguage() || 'vi';
+    const selectEl = document.getElementById('select_change_language');
+    document.querySelectorAll('#select_change_language option[data-key]').forEach(opt => {
+        let key = opt.attributes['data-key']?.value;
+        if (key === language) opt.selected = true;
+    });
+    selectEl.addEventListener('change', () => {
+        // const optionSelected = selectEl?.options[selectEl?.selectedIndex];
+        onChangeLanguage(selectEl.value);
+    })
+
+}
+
+const onChangeLanguage = (language) => {
+    localStorage.setItem('LANGUAGE', language);
+    translate();
+}
+
+// Init
+const init = () => {
+    translate();
+    setLinkToButtonRegister();
+    onSetLinkRedirectWeb();
+    initSelectElement();
+}
+
+window.onload = () => {
+    init();
+}
+addEventListener('DOMContentLoaded', (event) => {
+    init();
+});
+
+
+
+
+
---
+++ js/translateKey.js
@@ -0,0 +1,147 @@
+export const keyLanguage = {
+    'common_text_link_tictop_website': {
+        'en': 'tictop.vn',
+        'fr': 'tictop.eu',
+        'vi': 'tictop.vn',
+    },
+    'common_text_language_vietnamese': {
+        'en': 'Vietnamese',
+        'fr': 'Vietnamien',
+        'vi': 'Tiếng Việt',
+    },
+    'common_text_language_english': {
+        'en': 'English',
+        'fr': 'Anglais',
+        'vi': 'Tiếng Anh',
+    },
+    'common_text_language_france': {
+        'en': 'French',
+        'fr': 'Français',
+        'vi': 'Tiếng Pháp',
+    },
+    'common_text_download_now': {
+        'en': 'Download Now ↓',
+        'fr': 'Télécharger maintenant ↓',
+        'vi': 'Tải ngay ↓'
+    },
+    'common_name_company': {
+        'en': 'VIG TECHNOLOGY., JSC',
+        'fr': 'VIG TECHNOLOGY., JSC',
+        'vi': 'CÔNG TY CỔ PHẦN CÔNG NGHỆ VÀ TIN HỌC VIG'
+    },
+    'main_title_introduce': {
+        'en': 'Task management software improves productivity by 5 times',
+        'fr': 'Les logiciels de gestion des tâches améliorent la productivité de 5 fois',
+        'vi': 'Phần mềm quản lý công việc nâng cao hiệu suất gấp 5 lần'
+    },
+    'main_sub_title_introduce': {
+        'en': 'Use time simply, effectively, easily, optimally...',
+        'fr': 'Utiliser le temps simplement, efficacement, facilement, de manière optimale...',
+        'vi': 'Sử dụng thời gian đơn giản, hiệu quả, dễ dàng, tối ưu...'
+    },
+    'feature_time_manager_title': {
+        'en': 'Time management',
+        'fr': 'Gestion du temps',
+        'vi': 'Quản lý thời gian'
+    },
+    'feature_time_manager_sub_title': {
+        'en': 'You can always control your working time and work in a day!',
+        'fr': 'Vous pouvez toujours contrôler votre temps de travail et travailler en une journée!',
+        'vi': 'Bạn luôn có thể kiểm soát thời gian làm việc và công việc trong một ngày!'
+    },
+    'feature_time_manager_description_1': {
+        'en': 'Track work and work time in a day',
+        'fr': 'Suivre le travail et le temps de travail dans une journée',
+        'vi': 'Theo dõi công việc và thời gian làm việc trong một ngày'
+    },
+    'feature_time_manager_description_2': {
+        'en': 'Manage each employee\'s time effectively',
+        'fr': 'Gérez efficacement le temps de chaque employé',
+        'vi': 'Quản lý thời gian của từng nhân viên một cách hiệu quả'
+    },
+    'feature_time_manager_description_3': {
+        'en': 'Work time tracking',
+        'fr': 'Suivi du temps de travail',
+        'vi': 'Theo dõi thời gian làm việc'
+    },
+    'feature_task_manager_title': {
+        'en': 'Workflow management',
+        'fr': 'Gestion des flux de travail',
+        'vi': 'Quản lý công việc'
+    },
+    'feature_task_manager_sub_title': {
+        'en': 'Quick, simple job creation with all you need!',
+        'fr': 'Création d\'emploi rapide et simple avec tout ce dont vous avez besoin !',
+        'vi': 'Tạo công việc nhanh chóng, đơn giản với tất cả những gì bạn cần!'
+    },
+    'feature_task_manager_description_1': {
+        'en': 'Keep track of the work of everyone in the team that needs to be handled',
+        'fr': 'Gardez une trace du travail de chaque membre de l\'équipe qui doit être géré',
+        'vi': 'Theo dõi công việc của mọi người trong nhóm cần xử lý'
+    },
+    'feature_task_manager_description_2': {
+        'en': 'Reduce work exchange time',
+        'fr': 'Réduire le temps d\'échange de travail',
+        'vi': 'Giảm thời gian trao đổi công việc'
+    },
+    'feature_task_manager_description_3': {
+        'en': 'Keep track of each person\'s work progress',
+        'fr': 'Suivre l\'avancement du travail de chacun',
+        'vi': 'Theo dõi tiến độ công việc của mỗi người'
+    },
+    'feature_task_manager_description_4': {
+        'en': 'Exchange, make online calls to exchange work',
+        'fr': 'Exchange, passer des appels en ligne pour échanger du travail',
+        'vi': 'Trao đổi, thực hiện các cuộc gọi trực tuyến trao đổi công việc'
+    },
+    'feature_task_manager_description_5': {
+        'en': 'Manage and store your files and resources',
+        'fr': 'Gérez et stockez vos fichiers et ressources',
+        'vi': 'Quản lý, lưu trữ file và tài nguyên của bạn'
+    },
+    'card_start_try_title': {
+        'en': 'Start the experience!',
+        'fr': 'Commencez l\'expérience!',
+        'vi': 'Bắt đầu trải nghiệm!'
+    },
+    'card_start_try_sub_title': {
+        'en': 'Experience a new way of working, saving and more!',
+        'fr': 'Découvrez une nouvelle façon de travailler, d\'économiser et plus encore!',
+        'vi': 'Trải nghiệm một cách làm việc mới, tiết kiệm và hơn thế nữa!'
+    },
+    'footer_copy_right_text': {
+        'en': 'Copyright © 2023 VIG | Powered by VIG',
+        'fr': 'Copyright © 2023 VIG | Propulsé par VIG',
+        'vi': '© 2023 - Bản quyền thuộc về Công ty Cổ Phần Công Nghệ và Tin Học VIG'
+    },
+    'intro_overview_subtitle': {
+        'en': "TOP WORK MANAGEMENT SOFTWARE <b class='text-highlight'>FLEXIBLE</b>",
+        'fr': "TOP LOGICIEL DE GESTION DU TRAVAIL <b class='text-highlight'>FLEXIBLE</b>",
+        'vi': "PHẦM MỀM QUẢN LÝ CÔNG VIỆC <b class='text-highlight'>LINH HOẠT</b> HÀNG ĐẦU",
+    },
+    'intro_overview_title_text_trust_by': {
+        'en': "Trusted by",
+        'fr': "Approuvé par",
+        'vi': "Được tin dùng bởi",
+    },
+    'intro_overview_title_text_businesses': {
+        'en': "businesses",
+        'fr': "entreprises",
+        'vi': "doanh nghiệp",
+    },
+    'intro_overview_content_text_description': {
+        'en': "Simple and flexible with many models – business sizes, Tictop is the ideal choice for businesses who are having difficulty in finding a software <b style=\"color:#008d8d;\">FIT </b> with me and <b style=\"color:#008d8d;\">INHANCED</b> work efficiency",
+        "fr": "Simple et flexible avec de nombreux modèles – tailles d'entreprises, Tictop est le choix idéal pour les entreprises qui ont du mal à trouver un logiciel <b style=\"color:#008d8d;\">FIT </b> avec moi et <b style=\" color:#008d8d;\">Amélioration</b> de l'efficacité du travail",
+        "vi": "Đơn giản và linh hoạt với nhiều mô hình – quy mô doanh nghiệp, Tictop là lựa chọn lý tưởng cho những doanh nghiệp đang gặp khó khăn trong việc tìm kiếm một phần mềm <b style=\"color:#008d8d;\">PHÙ HỢP</b> với mình và <b style=\"color:#008d8d;\">NÂNG CAO</b> hiệu quả công việc"
+    },
+    'intro_overview_content_text_description_1': {
+        'en': 'Customers love Tictop because of its simplicity and efficiency',
+        'fr': 'Les clients aiment Tictop pour sa simplicité et son efficacité',
+        'vi': 'KH yêu thích Tictop vì sự đơn giản và hiệu quả',
+    },
+    'intro_overview_content_text_description_2': {
+        'en': 'Customer has recommended Tictop to friends and partners',
+        'fr': 'Le client a recommandé Tictop à des amis et partenaires',
+        'vi': 'KH đã giới thiệu Tictop cho bạn bè và đối tác',
+    },
+}
